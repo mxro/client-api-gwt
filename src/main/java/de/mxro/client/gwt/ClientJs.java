00f291f1f7e042f5ad699a84b07504334882b517
mxro <mxro@nowhere.com>
2015-01-15 15:51:27 +1100

    Devloping first implementation of client-api-gwt Autocommit Thu Jan 15 15:51:27 EST 2015

--- src/main/java/de/mxro/client/gwt/ClientJs.java
+++ src/main/java/de/mxro/client/gwt/ClientJs.java
@@ -1,5 +1,13 @@
 package de.mxro.client.gwt;
 
-public class ClientJs {
+import org.timepedia.exporter.client.Export;
+import org.timepedia.exporter.client.Exportable;
+
+@Export
+public class ClientJs implements Exportable {
+
+    public ClientJs() {
+        super();
+    }
 
 }
