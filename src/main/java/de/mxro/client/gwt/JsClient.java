b8c3c129fcb186e59235729f4df625187da7b9a7
mxro <mxro@nowhere.com>
2015-07-08 18:15:24 +1000

    Moving projects to Java Delight Autocommit Wed Jul 08 18:15:24 EST 2015

--- src/main/java/de/mxro/client/gwt/JsClient.java
+++ src/main/java/de/mxro/client/gwt/JsClient.java
@@ -1,6 +1,7 @@
 package de.mxro.client.gwt;
 
-import org.javadelight.log.js.JsLogsNode;
+import delight.log.js.JsLogsNode;
+
 import org.timepedia.exporter.client.Export;
 import org.timepedia.exporter.client.Exportable;
 import org.timepedia.exporter.client.NoExport;
